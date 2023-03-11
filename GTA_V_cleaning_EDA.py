59f744457fc9971a525ac89bb7841a1a91032352
CarlosGG18 <carlos.govea@baruchmail.cuny.edu>
2023-03-11 12:12:21 -0500

    cleaning

--- GTA_V_cleaning_EDA.py
+++ GTA_V_cleaning_EDA.py
@@ -13,9 +13,27 @@ import nltk
 from nltk import stopwords
 from nltk.tokenize import regexp_tokenize, word_tokenize, RegexpTokenizer
 from nltk.probability import FreqDist
-# nltk.download('punkt') #word_tokenize wont work without
+nltk.download('punkt') #word_tokenize wont work without
 
 df = pd.read_csv('GTA_V.csv')
 df.info()
-test_review = df['review'].loc[200]
-test_review
+df['received_for_free'].value_counts(normalize=True)
+df['voted_up'].value_counts(normalize=True)
+df['weighted_vote_score'].value_counts(normalize=True) # Weighted vote score severly imbalanced with 88%+ in 0
+
+df['author'][0]
+
+
+#Want key, value pair within Author column to be its own columns, so need to extract 
+
+def extract_author_data(author_string):
+    author_dict = eval(author_string)  # convert the string to a dictionary
+    return author_dict
+
+df['author_data'] = df['author'].apply(extract_author_data)
+df = pd.concat([df.drop(['author_data'], axis=1), df['author_data'].apply(pd.Series)], axis=1)
+df = df.drop(['author'], axis=1)
+
+df.drop(['steam_china_location'], axis=1, inplace=True)
+df.dropna(inplace=True)
+df.info()
